9de2747292403998b40201347e65cff0934a5d91
muktermolla016 <muktermolla016@gmail.com>
2025-12-02 20:45:21 +0600

    Add files via upload

--- config.py
+++ config.py
@@ -3,11 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "0"))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "26106561"))
+API_HASH = getenv("API_HASH", "6bf2b0a825260beafe9955ffed29f978")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "0"))
+BOT_TOKEN = getenv("BOT_TOKEN", "7550184093:AAEwTZ-19Vgl6ax7VfpS0Tawe43LmTu0glQ")
+OWNER_ID = int(getenv("OWNER_ID", "7753899951"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = "mongodb+srv://demo38174_db_user:myhTrNGcYm5zpQus@cluster0.dboqmtf.mongodb.net/?appName=Cluster0")
+MUST_JOIN = getenv("MUST_JOIN", main_channel_pr)
+
+
