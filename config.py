d338348a91aba44ed9511d120d915d74bbc51953
muktermolla016 <muktermolla016@gmail.com>
2025-12-02 21:31:47 +0600

    Fix formatting of MONGO_DB_URI in config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_HASH = getenv("API_HASH", "6bf2b0a825260beafe9955ffed29f978")
 BOT_TOKEN = getenv("BOT_TOKEN", "7550184093:AAEwTZ-19Vgl6ax7VfpS0Tawe43LmTu0glQ")
 OWNER_ID = int(getenv("OWNER_ID", "7753899951"))
 
-MONGO_DB_URI = "mongodb+srv://demo38174_db_user:myhTrNGcYm5zpQus@cluster0.dboqmtf.mongodb.net/?appName=Cluster0")
+MONGO_DB_URI = "mongodb+srv://demo38174_db_user:myhTrNGcYm5zpQus@cluster0.dboqmtf.mongodb.net/?appName=Cluster0"
 MUST_JOIN = getenv("MUST_JOIN", main_channel_pr)
 
 
